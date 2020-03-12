013559220da4e3a8e6f7adad60a8174b78963c48
Zafar Imam <zafarimam512@gmail.com>
2020-03-13 00:13:51 +0530

    for loops updated

--- src/loop_iterators/WhileDoWhile.kt
+++ src/loop_iterators/WhileDoWhile.kt
@@ -2,13 +2,6 @@ package com.example.kotlin.LoopOrIterators
 
 fun main(args: Array<String>) {
 
-    //for loop
-   /* for (i in 1..10){
-        if (i %2 == 0){
-            println("EvenNumbers $i")
-        }
-    }*/
-
     //while loop
     var i = 1
    /* while (i < 10){
